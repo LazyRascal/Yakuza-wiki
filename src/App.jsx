0e8a6f9e35ad8b63a0533e39fb66b90447e39d75
24webste <26882276-24webste@users.noreply.replit.com>
2024-01-12 14:51:00 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+function App() {
+  return (
+    <div className='title'>
+      <h1>Yakuza Wiki</h1>
+    
+      <a href='Yakuza Kiwami'><span>Yakuza Kiwami</span></a>
+
+      <a href='Yakuza kiwami 2'><span>Yakuza kiwami 2</span></a>
+
+      <a href='Yakuza 3'><span>Yakuza 3</span></a>
+
+      <a href='Yakuza 4'><span>Yakuza 4</span></a>
+
+      <a href='Yakuza 5'><span>Yakuza 5</span></a>
+      
+      <a href='Yakuza 6'><span>Yakuza 6</span></a>
+
+      <a href='Yakuza 0'><span>Yakuza 0</span></a>
+
+      <a href='Yakuza Ishin'><span>Yakuza Ishin</span></a>
+
+      <a href='Judgement'><span>Judgement</span></a>
+
+      <a href='Lost Judgement'><span>Lost Judgement</span></a>
+
+      <a href='Yakuza Like A Dragon'><span>Yakuza Like A Dragon</span></a>
+
+      <a href='Like A Dragon Gaiden'><span>Like A Dragon Gaiden</span></a>
+      
+      <div className="triangle-down"></div>
+    </div>
+)
+}
